5261a6767de83cf2db2ce89d47d1c16d58e1e159
BryanBorgesLima <bryanlima92000@gmail.com>
2026-01-25 17:19:47 +0000

    boboa

--- dicionarios/exerc91.py
+++ dicionarios/exerc91.py
@@ -1,13 +1,12 @@
-import random
-jogadores = {'jogaor1': '', 'jogaor2': '', 'jogaor3': '', 'jogaor4': '' }
-
-for chave in jogadores:
-    jogadores[chave] = random.sample(range(1, 7), 1)
+from random import randint
+from operator import itemgetter
+jogadores = {'jogaor1': randint(1, 6), ''
+             'jogaor2': randint(1, 6), 
+             'jogaor3': randint(1, 6), 
+             'jogaor4': randint(1, 6) }
 for k, v in jogadores.items():
-    print(f'O {k} tirou', *v)
-'''''
-ordena = dict(
-    sorted(jogadores.items(), key=lambda item: item[1], reverse=True)
-)
-'''
+    print(f'O {k} tirou {v}')
 
+ranking = list()
+ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+print(ranking)
